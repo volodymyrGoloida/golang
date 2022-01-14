bbe6da2ddfbca1bcfeb256131a1c8a3566f7683b
unknown <Volodymyr Goloida>
2022-01-14 03:43:25 +0200

    add:work with execute and temlpates

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -13,13 +14,19 @@ type User struct {
 }
 
 func (u User) getAllInfo() string {
-	return fmt.Sprintf("User name is: %s  He is %d and he has money equal %d", u.name,
+	return fmt.Sprintf("User name is: %s  He is %d and he has money"+
+		"equal %d", u.name,
 		u.age, u.money)
+}
+func (u *User) setNewName(newName string) { //силку передаю за допомогою *
+	u.name = newName
 }
 func home_page(w http.ResponseWriter, r *http.Request) {
 	bob := User{"Boob", 25, -50, 4.2, 0.7}
-	bob.name = "volodymyr"
-	fmt.Fprintf(w, "username is:"+bob.name)
+	// bob.setNewName("andrew")
+	// fmt.Fprintf(w, bob.getAllInfo())
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
 }
 func contacts_page(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "contacts page")
